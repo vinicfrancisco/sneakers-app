6292460a07db27d1ddac60a1b7fab9ce36cd0180
Vinícius C. Francisco <vini.francisco@more.tt>
2023-07-30 16:27:22 -0300

    refactor: input component

--- components/basic/Input/index.tsx
+++ components/basic/Input/index.tsx
@@ -1,30 +1,35 @@
-import tokens from '@/assets/theme/tokens'
-import Feather from '@expo/vector-icons/Feather'
 import { useState } from 'react'
-import { TextInputProps } from 'react-native'
-import { GetProps, Stack, Input as TamaguiInput, useTheme } from 'tamagui'
+import { TextInput, TextInputProps } from 'react-native'
+import Feather from '@expo/vector-icons/Feather'
+import { XStack, styled, useTheme } from 'tamagui'
 
 interface InputProps extends TextInputProps {
   search?: boolean
 }
 
-function Input({ search, placeholder }: InputProps) {
+const TamaguiInput = styled(TextInput, {
+  name: 'Input',
+
+  f: 1,
+  p: '$4',
+  color: '$primary',
+  fontFamily: '$body',
+  placeholderTextColor: '$secondary',
+})
+
+function Input({ search, ...props }: InputProps) {
   const { icon, primary } = useTheme()
 
   const [isFocused, setIsFocused] = useState<boolean>(false)
 
   return (
-    <Stack
-      flexDirection="row"
+    <XStack
       ai="center"
-      px="$4"
-      borderWidth={isFocused ? 2 : 0}
-      borderColor="$black"
-      bg="$gray1"
-      mx="$4"
-      h="8%"
-      mb="$6"
+      borderWidth={2}
+      borderColor={isFocused ? '$primary' : 'transparent'}
+      bg="$componentBackground"
       br="$3"
+      paddingLeft="$4"
     >
       {search && (
         <Feather
@@ -35,22 +40,12 @@ function Input({ search, placeholder }: InputProps) {
       )}
 
       <TamaguiInput
-        fontFamily="$heading"
-        size="25%"
-        bg="$gray1"
-        marginLeft="$4"
-        borderWidth={0}
+        {...props}
         onBlur={() => setIsFocused(false)}
         onFocus={() => setIsFocused(true)}
-        placeholderTextColor="$gray2"
-        f={1}
-        fs="$4"
-        placeholder={placeholder}
       />
-    </Stack>
+    </XStack>
   )
 }
-
-export type HeadingProps = GetProps<typeof Input>
 
 export default Input
