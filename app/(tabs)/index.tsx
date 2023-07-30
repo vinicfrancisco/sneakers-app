32a5312bd889b10f2f7f9d05d1e99ce0a45849f1
Naua-stack <nauafernandesvv@gmail.com>
2023-07-30 11:54:42 -0300

    feat: add title of sneakers list

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,16 +1,27 @@
 import SneakerCard from '@/components/SneakerCard'
+import { Heading } from '@/components/basic/Heading'
 import { FlashList } from '@shopify/flash-list'
-import { Stack } from 'tamagui'
+import { Stack, getTokens } from 'tamagui'
 
 const data = new Array(50).fill(0)
 
 export default function Home() {
+  const { space } = getTokens()
+
   return (
-    <Stack f={1} bg="$bg">
+    <Stack f={1} bg="$bg" padding="$4">
+      <Heading mx="$4" color="$subTitle" fs="$2">
+        Most Popular
+      </Heading>
+
       <FlashList
         horizontal
+        ItemSeparatorComponent={() => <Stack width="$8" />}
         estimatedItemSize={187}
         data={data}
+        contentContainerStyle={{
+          padding: space[4].val,
+        }}
         renderItem={() => <SneakerCard />}
       />
     </Stack>
