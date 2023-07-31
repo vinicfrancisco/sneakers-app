5adba381dc3a25d7fb6759a917837f69435e32a3
Vinícius C. Francisco <vini.francisco@more.tt>
2023-07-31 20:02:05 -0300

    feat: create home header component and add toggle theme button

---
+++ components/HomeHeader/index.tsx
@@ -0,0 +1,38 @@
+import { TouchableOpacity } from 'react-native'
+import Feather from '@expo/vector-icons/Feather'
+import { useTheme, YStack, XStack, Heading } from 'tamagui'
+import useAppTheme from '~/hooks/useTheme'
+import Input from '../basic/Input'
+
+export default function HomeHeader() {
+  const { primary } = useTheme()
+  const { theme, toggleTheme } = useAppTheme()
+
+  return (
+    <YStack mx="$4" mb="$8">
+      <XStack jc="space-between">
+        <Heading fs={'$5'} mb="$4">
+          Search products
+        </Heading>
+
+        <TouchableOpacity
+          onPress={toggleTheme}
+          hitSlop={{
+            top: 8,
+            bottom: 8,
+            left: 8,
+            right: 8,
+          }}
+        >
+          {theme === 'dark' ? (
+            <Feather name="sun" size={24} color={primary.get()} />
+          ) : (
+            <Feather name="moon" size={24} color={primary.get()} />
+          )}
+        </TouchableOpacity>
+      </XStack>
+
+      <Input search placeholder='Try "Yeezy boost 350"' />
+    </YStack>
+  )
+}
