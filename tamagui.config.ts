cb1ec7cebb7d7ccda4f2671c7c9487aa89d1bbb5
Vinícius C. Francisco <vini.francisco@more.tt>
2023-07-31 20:00:37 -0300

    refactor: export each theme on app assets

--- tamagui.config.ts
+++ tamagui.config.ts
@@ -1,8 +1,9 @@
 import { themes } from '@tamagui/themes'
 import { createTamagui } from 'tamagui'
-import { body, heading } from './assets/theme/fonts'
-import shorthands from './assets/theme/shorthands'
-import tokens from './assets/theme/tokens'
+import { dark, light } from './assets/themes'
+import { body, heading } from './assets/themes/utils/fonts'
+import shorthands from './assets/themes/utils/shorthands'
+import tokens from './assets/themes/utils/tokens'
 
 const config = createTamagui({
   fonts: {
@@ -11,22 +12,8 @@ const config = createTamagui({
   },
   themes: {
     ...themes,
-    light: {
-      ...themes.light,
-      background: tokens.color.gray1,
-      componentBackground: tokens.color.white,
-      primary: tokens.color.black,
-      secondary: tokens.color.gray3,
-      icon: tokens.color.gray2,
-    },
-    dark: {
-      ...themes.dark,
-      background: tokens.color.black,
-      componentBackground: tokens.color.gray4,
-      primary: tokens.color.gray1,
-      secondary: tokens.color.gray2,
-      icon: tokens.color.gray2,
-    },
+    light,
+    dark,
   },
   tokens,
   shorthands,
