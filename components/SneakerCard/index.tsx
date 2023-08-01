3bf26fba5adfce253a049230b8eec43cc905f5a8
Vinícius C. Francisco <vini.francisco@more.tt>
2023-08-01 18:51:40 -0300

    feat: add brand on sneaker card

--- components/SneakerCard/index.tsx
+++ components/SneakerCard/index.tsx
@@ -3,6 +3,7 @@ import Feather from '@expo/vector-icons/Feather'
 import { Image, Stack, XStack, YStack, useTheme } from 'tamagui'
 import Heading from '~/components/basic/Heading'
 import { ISneaker } from '~/domain/sneakers'
+import Text from '../basic/Text'
 
 interface ISneakerCardProps {
   data: ISneaker
@@ -10,12 +11,12 @@ interface ISneakerCardProps {
 
 const SCREEN_WIDTH = Dimensions.get('window').width
 export const CARD_WIDTH = SCREEN_WIDTH * 0.6
-const CARD_IMAGE_HEIGHT = 180
+const CARD_IMAGE_HEIGHT = 160
 const CARD_BUTTON_WIDTH = CARD_WIDTH * 0.4
 const CARD_BUTTON_HEIGHT = 70
 
 export default function SneakerCard({ data }: ISneakerCardProps) {
-  const { name, image, retailPrice } = data
+  const { name, brand, image, retailPrice } = data
 
   const { background } = useTheme()
 
@@ -32,16 +33,20 @@ export default function SneakerCard({ data }: ISneakerCardProps) {
             {name}
           </Heading>
 
-          <Image
-            alt="Air Jordan 1 Travis Scott OG"
-            h={CARD_IMAGE_HEIGHT}
-            my="$8"
-            resizeMode="contain"
-            source={{
-              uri: image.original,
-            }}
-          />
+          <Text fs="$3" numberOfLines={1} color="$secondary" mt="$1">
+            {brand}
+          </Text>
         </YStack>
+
+        <Image
+          alt="Air Jordan 1 Travis Scott OG"
+          h={CARD_IMAGE_HEIGHT}
+          my="$8"
+          resizeMode="contain"
+          source={{
+            uri: image.original,
+          }}
+        />
 
         <XStack ai="center">
           <Heading f={1} fs="$5" px="$4">
