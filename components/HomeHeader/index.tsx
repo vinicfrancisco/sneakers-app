b459f7c38a2b09f08a34b26ca70b06f8430af204
Vinícius C. Francisco <vini.francisco@more.tt>
2023-08-10 18:56:50 -0300

    chore: remove toggle theme button

--- components/HomeHeader/index.tsx
+++ components/HomeHeader/index.tsx
@@ -1,34 +1,10 @@
-import { TouchableOpacity } from 'react-native'
-import Feather from '@expo/vector-icons/Feather'
-import theme from '~/assets/theme'
-import useAppTheme from '~/hooks/useTheme'
-import { HStack, Heading, VStack } from '../core'
+import { Heading, VStack } from '../core'
 import Input from '../core/Input'
 
 export default function HomeHeader() {
-  const { colorMode, toggleTheme } = useAppTheme()
-
   return (
     <VStack mx="$medium">
-      <HStack justifyContent="space-between">
-        <Heading mb="$medium">Search products</Heading>
-
-        <TouchableOpacity
-          onPress={toggleTheme}
-          hitSlop={{
-            top: 8,
-            bottom: 8,
-            left: 8,
-            right: 8,
-          }}
-        >
-          {colorMode === 'dark' ? (
-            <Feather name="sun" size={24} color={theme.colors.white} />
-          ) : (
-            <Feather name="moon" size={24} color={theme.colors.black} />
-          )}
-        </TouchableOpacity>
-      </HStack>
+      <Heading mb="$medium">Search products</Heading>
 
       <Input search placeholder='Try "Yeezy boost 350"' />
     </VStack>
