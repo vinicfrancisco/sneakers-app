70e275ada69a5976d3ec50a0993164bdeff4cd90
Vinícius C. Francisco <vinicfrancisco@gmail.com>
2023-08-14 16:39:00 -0300

    chore: add temporary sign out button

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -3,14 +3,16 @@ import { StyleSheet } from 'react-native'
 import { useSafeAreaInsets } from 'react-native-safe-area-context'
 import { FlashList } from '@shopify/flash-list'
 import { BOTTOM_TABS_HEIGHT } from '~/components/BottomTabs'
-import { Stack } from '~/components/core'
+import { Button, Stack } from '~/components/core'
 import Heading from '~/components/core/Heading'
 import ScrollView from '~/components/core/ScrollView'
 import HomeHeader from '~/components/HomeHeader'
 import SneakerCard, { CARD_WIDTH } from '~/components/SneakerCard'
 import UpcomingCard from '~/components/UpcomingCard'
+import { AuthActions } from '~/store/features/auth/slice'
 import theme from '~/assets/theme'
 import { ISneaker } from '~/domain/sneakers'
+import { useAppDispatch } from '~/hooks'
 
 const mock: ISneaker[] = [
   {
@@ -316,6 +318,7 @@ const mock: ISneaker[] = [
 ]
 
 export default function Home() {
+  const dispatch = useAppDispatch()
   const { bottom, top } = useSafeAreaInsets()
 
   return (
@@ -329,6 +332,11 @@ export default function Home() {
         }}
       >
         <HomeHeader />
+
+        {/* Temporary */}
+        <Button.Container onPress={() => dispatch(AuthActions.signOut())}>
+          <Button.Text>Sign out</Button.Text>
+        </Button.Container>
 
         <Heading mx="$medium" my="$large" color="$gray5" fs="$medium">
           New Releases
