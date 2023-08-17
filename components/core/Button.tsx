e3e23371cb312c6273e1d3d62cff3477d3bf32e4
Vinícius C. Francisco <vinicfrancisco@gmail.com>
2023-08-17 13:08:21 -0300

    refactor: button component

--- components/core/Button.tsx
+++ components/core/Button.tsx
@@ -1,6 +1,12 @@
-import { TouchableOpacity } from 'react-native'
+import { TouchableOpacity, TouchableOpacityProps } from 'react-native'
 import { styled } from '@gluestack-style/react'
 import Heading from './Heading'
+import Loading from './Loading'
+
+interface ButtonProps extends TouchableOpacityProps {
+  loading?: boolean
+  children: React.ReactNode
+}
 
 const Container = styled(
   TouchableOpacity,
@@ -11,6 +17,14 @@ const Container = styled(
     justifyContent: 'center',
     padding: '$medium',
     width: '100%',
+
+    variants: {
+      loading: {
+        true: {
+          opacity: 0.3,
+        },
+      },
+    },
   },
   {
     descendantStyle: ['_text'],
@@ -26,9 +40,15 @@ const Text = styled(
   { ancestorStyle: ['_text'] },
 )
 
-const Button = {
-  Container,
-  Text,
+function Button({ children, loading, ...props }: ButtonProps) {
+  return (
+    <Container {...props} loading={loading ? 'true' : undefined}>
+      {loading ? <Loading /> : <Text>{children}</Text>}
+    </Container>
+  )
 }
+
+Button.Container = Container
+Button.Text = Text
 
 export default Button
