0bfcbcf61c77f533dd6d1d32ba9b6b7e7b9e8554
Vinícius C. Francisco <vinicfrancisco@gmail.com>
2023-08-18 17:26:27 -0300

    chore: add rapid api envs

--- @types/env.d.ts
+++ @types/env.d.ts
@@ -1,4 +1,10 @@
 declare module '@env' {
+  // Supabase
   export const SUPABASE_URL: string
   export const SUPABASE_KEY: string
+
+  // Rapid API
+  export const RAPID_API_URL: string
+  export const RAPID_API_KEY: string
+  export const RAPID_API_HOST: string
 }
